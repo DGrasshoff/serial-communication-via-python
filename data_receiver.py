8e86cc013f8aaef8bc9206e73b675b1ef871e5e3
Daniel Grasshoff <daniel.grasshoff@icloud.com>
2023-09-17 11:34:22 +0200

    quality of life features

--- data_receiver.py
+++ data_receiver.py
@@ -8,7 +8,6 @@ arduino_port = '/dev/cu.usbmodem11301'  # Replace with your Arduino's port
 ser = serial.Serial(arduino_port, 9600)
 
 file_path = '/Users/daniel/Documents/Schule/Physik_Z/arduino_data/'
-state_string = "stop"
 time_increment = 1 / 1000
 time_duration = 10
 file_name = None  # Initialize file_name as None
@@ -27,46 +26,65 @@ def update_plot():
     plt.pause(time_increment/1000000000)  # Pause for a short time to update the plot
 
 while True:
-    user_input = input("Press 's' to start data collection\nPress t for a new time duration\nPress 'c' to change the file name\nPress 'q' to end the program  " + '\n')
+    #gives information about the plot
+    print("\nPress 's' to start data collection")
+    print("Press 't' for a new time duration")
+    print("Press 'c' to change the file name")
+    print("Press 'q' to end the program")
+    user_input = input("Input: ")
     
+    #checks whether it should start saving data
     if user_input == 's':
+        #checks whether a file name is given
         if file_name is None:
-            print("Please set a file name using 'c' command first.")
-        else:
-            # Open the CSV file for writing
-            os.mkdir(file_path + file_name)
-            csv_file_path = file_path + file_name + "_data.csv"
-            start_time = time.time()
-            counter = 0
-            with open(csv_file_path, 'w', newline='') as csvfile:
-                csv_writer = csv.writer(csvfile)
-                csv_writer.writerow(["Time (s)", "Data"])  # Write header row
+            file_name = input("Set a file name: ")
 
-                # Clear previous data
-                time_values.clear()
-                data_values.clear()
-                while True:
-                    data = ser.readline().decode('utf-8').strip()
-                    csv_writer.writerow([time.time()- start_time, data])
-                    # Append time and data values for plotting
-                    time_values.append(time.time()- start_time)
-                    data_values.append(float(data))  # Assuming data is a float
-                    update_plot()
-                    counter = counter + 1
-                    time.sleep(time_increment)
-                    #looks whether time has passed
-                    if time.time() - start_time >= time_duration:
-                        break
-                
-                print("Data collection finished")
-                print("loop has run for: " + str(time.time() - start_time) + " seconds\n")
-                print("loop has run " + str(counter) + " times\n")
-                file_name = None
-                
+        #creates a new directory
+        os.mkdir(file_path + file_name)
+        #creates a new csv file for wrting
+        csv_file_path = file_path + file_name + "/" + file_name + "data.csv"
+        #saves the starting time
+        start_time = time.time()
+        #resets the counter
+        counter = 0
+        #opens the csv file
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["Time (s)", "Data"])  # Write header row
+
+            # Clear previous data
+            time_values.clear()
+            data_values.clear()
+            while True:
+                #reads the data
+                data = ser.readline().decode('utf-8').strip()
+                #writes the data to the csv file
+                csv_writer.writerow([time.time()- start_time, data])
+                # Append time and data values for plotting
+                time_values.append(time.time()- start_time)
+                data_values.append(float(data))  # Assuming data is a float
+                #updates the plot
+                update_plot()
+                #counts the number of cycles
+                counter = counter + 1
+                #looks whether time has passed
+                if time.time() - start_time >= time_duration:
+                    break
+            
+            #gives basic information
+            print("Data collection finished\n")
+            print("loop has run for: " + str(time.time() - start_time) + " seconds")
+            print("loop has run " + str(counter) + " times")
+            
+            #prevents the data from being overwritten
+            file_name = None
+    #configure the file name
     elif user_input == 'c':
         file_name = input("Enter a file name: ")
+    #configure the time duration
     elif user_input == 't':
         time_duration = int(input("Enter a new time duration(in s): "))
+    #exit the code
     elif user_input == 'q':
         break
 
